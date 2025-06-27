ea79423f363f52b9448cb8a969005d3e2b77e595
roohitmaity <ethicalhackernii@gmail.com>
2025-06-27 21:49:57 +0530

    update

---
+++ Code With Harry Python Full/chapter 4/problem_2.py
@@ -0,0 +1,19 @@
+marks=[]
+m1=input("enter 1st mark : ")
+marks.append(m1)
+m2=input("enter 2nd mark : ")
+marks.append(m2)
+m3=input("enter 3rd mark : ")
+marks.append(m3)
+m4=input("enter 4th mark : ")
+marks.append(m4)
+m5=input("enter 5th mark : ")
+marks.append(m5)
+m6=input("enter 6th mark : ")
+marks.append(m6)
+m7=input("enter 7th mark : ")
+marks.append(m7)
+
+marks.sort()
+
+print(marks)
---
+++ Code With Harry Python Full/chapter 4/problem_3.py
@@ -0,0 +1,3 @@
+t1=("rohit",22,22.0,True,'R')
+print(type(t1))
+t1[1]=21
